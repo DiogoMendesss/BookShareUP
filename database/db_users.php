c803594b13a9d934ef42888fa75cfc12825aa3bd
Knneves <fnneves9@gmail.com>
2023-12-16 20:21:47 +0000

    db user table now has password NN attribute and default status=active

--- database/db_users.php
+++ database/db_users.php
@@ -1,33 +1,33 @@
 <?php
 
   function loginSuccess($up_number, $password) {
-    global $bsup;
-    $stmt = $bsup->prepare('SELECT * FROM User WHERE up_number = ? AND password = ?');
+    global $dbh;
+    $stmt = $dbh->prepare('SELECT * FROM User WHERE up_number = ? AND password = ?');
     $stmt->execute(array($up_number, hash('sha256', $password)));
     return $stmt->fetch();
   }
 
   function mock_loginSuccess($up_number) {
-    global $bsup;
-    $stmt = $bsup->prepare('SELECT * FROM User WHERE up_number = ?');
+    global $dbh;
+    $stmt = $dbh->prepare('SELECT * FROM User WHERE up_number = ?');
     $stmt->execute(array($up_number));
     return $stmt->fetch();
   }
 
   function getUserFullName($up_number) {
-    global $bsup;
-    $stmt = $bsup->prepare('SELECT name FROM User WHERE up_number = ?');
+    global $dbh;
+    $stmt = $dbh->prepare('SELECT name FROM User WHERE up_number = ?');
     $stmt->execute(array($up_number));
     return $stmt->fetchColumn();
 }
 
 
   function insertUser($up_number, $password, $full_name, $faculty_campus) {
-    global $bsup;
-    $stmt = $bsup->prepare('INSERT INTO User (up_number, password, name) VALUES (?, ?, ?)');
+    global $dbh;
+    $stmt = $dbh->prepare('INSERT INTO User (up_number, password, name) VALUES (?, ?, ?)');
     $stmt->execute(array($up_number, password_hash($password, PASSWORD_DEFAULT), $full_name));
 
-    $stmt = $bsup->prepare('INSERT INTO UserCampus (user, campus) VALUES (?, ?)');
+    $stmt = $dbh->prepare('INSERT INTO UserCampus (user, campus) VALUES (?, ?)');
     $stmt->execute(array($up_number, $faculty_campus));
   }
 ?>
