8233c2d1530ec39cb6372f5290cfc8c3ef3a14eb
diogocerveira <119946039+diogocerveira@users.noreply.github.com>
2023-12-18 00:12:40 +0000

    Delete database directory

--- database/db_campus.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getCampusesInfo() {
-    global $dbh;
-    $stmt = $dbh->prepare('SELECT * FROM Campus');
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-?>
